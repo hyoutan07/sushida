a76ce5c0b16cdb59f927833eb81cef1726f05e7c
hyoutan07 <hyoutan0726@gmail.com>
2024-12-04 13:51:15 +0900

    update: ローマ字の複数判定アルゴリズム作成

--- sushi-front/src/components/feature/Nishida.tsx
+++ sushi-front/src/components/feature/Nishida.tsx
@@ -10,6 +10,12 @@ const Nishida = () => {
   // 入力文字関係
   const [currentWordIndex, setCurrentWordIndex] = useState(0); // 現在の単語インデックス
   const [typedWord, setTypedWord] = useState(''); // 現在の入力
+
+  // Romaji候補判定に使用する変数
+  const [candidateIndex, setCandidateIndex] = useState(0); // romaji候補リストのインデックス
+  const [typeCandidate, setTypeCandidate] = useState(""); // タイピングした候補文字列を一時保存
+  const [candidateIndexList, setCandidateIndexList] = useState<number[]>([]); // showRomajiでどのローマ字候補を通ったかを保存する変数
+
   // タイマー
   const [timeLeft, setTimeLeft] = useState(TIMER); // 残り時間
   // 画面表示
@@ -28,6 +34,7 @@ const Nishida = () => {
     audio.play();
   };
 
+  // TODO: 後できれいにする
   const handleKeyDown = (event: KeyboardEvent) => {
     if (!isGameStarted) {
       if (event.key === ' ' || event.key === 'Enter') {
@@ -40,30 +47,93 @@ const Nishida = () => {
       handleReset();
     }
 
-    const inputKey = event.key;
-    const currentRomaji = sushiList[currentWordIndex].romaji[0]; // 最初のローマ字だけを使う
-    // TODO: 「ちょ」cho tyoのような2通りの入力問題は、リストで管理ではなく、正規表現とかでできそうと思ったため一旦後回し
-
-    // 入力中の文字列を更新
-    const nextInput = typedWord + inputKey;
-
     if (isCompleted) return;
 
-    // 正しい入力が部分一致するかチェック
-    if (currentRomaji.startsWith(nextInput)) {
-      setTypedWord(nextInput);
-      setCorrectKeyCount((prev) => prev + 1);
-      playAudio('typing'); // 正しいキー入力時にタイピング音再生
+    const inputKey = event.key;
+    const currentRomajiCandidates = sushiList[currentWordIndex].romaji[candidateIndex]; // ローマ字の候補リスト
 
-      // 完全一致した場合
-      if (currentRomaji === nextInput) {
-        setScore((prev) => prev + 1);
-        playAudio('correct'); // 正解時に正解音再生
-        moveToNextWord();
+    // 入力中の文字列を更新
+    const nextInputAll = typedWord + inputKey; // 全体の文字列 
+    const nextCandidateInput = typeCandidate + inputKey; //候補入力の文字列 例: choko なら 「cho」, 「ko」の部分
+
+    console.log("入力", nextCandidateInput)
+
+    // nextCandidateInputの入力の正解判定に使用
+    let isCorrect = false;
+
+    // fixme: nが優先されてnnが許容されない
+    // 特別な処理: nの後にnnを許容
+    if (nextInputAll.endsWith('n') && inputKey === 'n' && candidateIndex > 0 && candidateIndex < sushiList[currentWordIndex].romaji.length - 1) {
+      const previousCandidateIndex = candidateIndex - 1
+      const previousRomajiCandidates = sushiList[currentWordIndex].romaji[previousCandidateIndex]; // 前回のローマ字の候補リスト
+      
+      for (let romajiIndex = 0; romajiIndex < previousRomajiCandidates.length; romajiIndex++) {
+        console.log(`候補 ${romajiIndex}: ${previousRomajiCandidates[romajiIndex]}`);
+        const romajiCandidate = previousRomajiCandidates[romajiIndex];
+
+        const extendedInput = "n" + inputKey;
+        if (romajiCandidate.startsWith(extendedInput)) {
+          setTypedWord(nextInputAll);
+          setTypeCandidate(extendedInput);
+          setCorrectKeyCount((prev) => prev + 1);
+          playAudio('typing');
+
+          isCorrect = true;
+
+          // TODO: clean up
+          const newCandidateIndexList = candidateIndexList.map((index, j) => (j === previousCandidateIndex ? romajiIndex : index))
+          console.log("候補インデックスリスト", newCandidateIndexList)
+          const newShowRomaji = newCandidateIndexList.map((index, i) => sushiList[currentWordIndex].romaji[i][index]).join('')
+          setCandidateIndexList(newCandidateIndexList)
+          setShowRomaji(newShowRomaji)
+
+          // romajiCandidateが全て正しく入力されていたら次へ
+          if (extendedInput.length === romajiCandidate.length) {
+            setTypeCandidate('');
+          }
+        }
       }
-    } else if (inputKey === ' ' || inputKey === 'Escape' || inputKey === 'Enter') {
-    } else {
-      // 間違ったキーが押された場合
+    }
+
+    for (let romajiIndex = 0; romajiIndex < currentRomajiCandidates.length; romajiIndex++) {
+      console.log(`候補 ${romajiIndex}: ${currentRomajiCandidates[romajiIndex]}`);
+      const romajiCandidate = currentRomajiCandidates[romajiIndex];
+
+      // 正しい入力が部分一致するかチェック
+      if (romajiCandidate.startsWith(nextCandidateInput)) {
+        setTypedWord(nextInputAll);
+        setTypeCandidate(nextCandidateInput);
+        setCorrectKeyCount((prev) => prev + 1);
+        playAudio('typing');
+
+        isCorrect = true;
+
+        // TODO: clean up
+        const newCandidateIndexList = candidateIndexList.map((index, j) => (j === candidateIndex ? romajiIndex : index))
+        console.log("候補インデックスリスト", newCandidateIndexList)
+        const newShowRomaji = newCandidateIndexList.map((index, i) => sushiList[currentWordIndex].romaji[i][index]).join('')
+        setCandidateIndexList(newCandidateIndexList)
+        setShowRomaji(newShowRomaji)
+
+        // romajiCandidateが全て正しく入力されていたら、次のRomaji候補へ進む
+        if (nextCandidateInput.length === romajiCandidate.length) {
+          setCandidateIndex((prev) => prev + 1)
+          setTypeCandidate("")
+
+          // 全単語の正解判定
+          if (candidateIndex + 1 === sushiList[currentWordIndex].romaji.length) {
+            setScore((prev) => prev + 1);
+            playAudio('correct');
+            moveToNextWord();
+          }
+        }
+        // if文を通った時点で他の候補は判定不要
+        break;
+      }
+    }
+
+    // 間違ったキーが押された場合
+    if (!isCorrect) {
       setMistypedKeyCount((prev) => prev + 1);
       playAudio('miss'); // ミスタイプ時にエラー音再生
     }
@@ -77,6 +147,7 @@ const Nishida = () => {
     setCorrectKeyCount(0);
     setMistypedKeyCount(0);
     setCurrentWordIndex(0);
+    setCandidateIndex(0);
     setTimeLeft(TIMER);
     setIsCompleted(false);
     moveToNextWord();
@@ -86,9 +157,12 @@ const Nishida = () => {
   const moveToNextWord = () => {
     const nextIndex = Math.floor(Math.random() * sushiList.length);
     setCurrentWordIndex(nextIndex);
+    setCandidateIndex(0);
     setTypedWord('');
     setMessage(`${sushiList[nextIndex].japanese}`);
-    setShowRomaji(`${sushiList[nextIndex].romaji[0]}`);
+    // setShowRomaji(`${sushiList[nextIndex].romaji}`);
+    setShowRomaji(sushiList[nextIndex].romaji.map(str => str[0]).join(''))
+    setCandidateIndexList(Array(sushiList[nextIndex].romaji.length).fill(0)) // romajiの長さ文の0配列で初期化
   };
 
   // リセット関数
@@ -98,6 +172,7 @@ const Nishida = () => {
     setCorrectKeyCount(0);
     setMistypedKeyCount(0);
     setCurrentWordIndex(0);
+    setCandidateIndex(0);
     setTimeLeft(TIMER);
     setMessage('スペースキーを押してゲーム開始！');
     setShowRomaji('');
@@ -178,6 +253,7 @@ const Nishida = () => {
           ))}
         </span>
       </div>
+      {/* デバック用 */} <h1 style={{ fontSize: '24px' }} >{typedWord} </h1>
       <p style={{ fontSize: '24px', marginTop: '20px', color: '#fff' }}>スコア: {score}</p>
       <p style={{ fontSize: '24px', marginTop: '20px', color: '#fff' }}>正しいキー入力数: {correctKeyCount}</p>
       <p style={{ fontSize: '24px', marginTop: '20px', color: '#fff' }}>ミスタイプ数: {mistypedKeyCount}</p>
